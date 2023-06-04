860eaba21a5f20315faf2403a7fa18d57ba2c56c
Malal0 <mj1055119@gmail.com>
2023-06-04 16:46:51 -0700

    updated

---
+++ galleryImages.js
@@ -0,0 +1,7 @@
+const images = [];
+
+for (let i = 1; i <= 9; i++) {
+    images.push(`img${i}.jpg`);
+}
+
+export default images;
--- main.js
+++ main.js
@@ -2,14 +2,22 @@
                     VARIABLES
 *************************************/
 
-
-
+import images from "./galleryImages.js";
+const galleryConatiner = get("#gallery-container");
 /*************************************
                     FUNCTIONS
 *************************************/
 
+function get(string) {
+    return document.querySelector(string);
+}
 
+function renderImages(arr) {
+    return arr.map(img => `<img class="slide" src="./assets/${img}" alt="${img}"/>`).join('');
+}
 
 /*************************************
-                    EVENT LISTENERS
+                    EVENT LISTENERS and ACTIONS
 *************************************/
+
+galleryConatiner.innerHTML = renderImages(images);
