b129c940bfa3960bbb5596ea9d36ab420f6fa3c3
Malal0 <mj1055119@gmail.com>
2023-06-04 21:41:20 -0700

    updated

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 import images from "./galleryImages.js";
 const galleryConatiner = get("#gallery-container");
+
 /*************************************
                     FUNCTIONS
 *************************************/
@@ -13,11 +14,39 @@ function get(string) {
 }
 
 function renderImages(arr) {
-    return arr.map(img => `<img class="slide" src="./assets/${img}" alt="${img}"/>`).join('');
+    return arr.map(img => {
+        const name = img.replace(".jpg", "");
+        return `<img class="slide" id="${name}" src="./assets/${img}" alt="${name}"/>`
+    }).join('');
+}
+
+function handleClick(e) {
+    const item = e;
+    console.log(item);
+}
+
+function handleGallery() {
+    const children = galleryConatiner.children;
+    console.log(children);
+    const firstChild = children[0];
+    // const lastChild = children[children.length - 1];
+    console.log(firstChild.offsetWidth);
+    // console.log(lastChild);
+
+    setInterval(() => {
+        galleryConatiner.scrollLeft += 100;
+        if (galleryConatiner.scrollLeft >= galleryConatiner.scrollLeftMax) {
+            galleryConatiner.scrollLeft = 0;
+        }
+    }, 1000);
 }
 
 /*************************************
                     EVENT LISTENERS and ACTIONS
 *************************************/
 
+addEventListener("click", handleClick);
+
 galleryConatiner.innerHTML = renderImages(images);
+
+handleGallery();
